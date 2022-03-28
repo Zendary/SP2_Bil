c6c881e119eea5be43753da0199e5c1f682d1bfb
Zendary <silverball1@gmail.com>
2022-03-28 11:28:31 +0200

    DieselTruck added

---
+++ src/com/company/DieselTruck.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class DieselTruck extends DieselCar{
+    public DieselTruck(String registrationNr, String make, String model, int amountOfDoors, int kmPrLitre, boolean particleFilter) {
+        super(registrationNr, make, model, amountOfDoors, kmPrLitre, particleFilter);
+    }
+}
